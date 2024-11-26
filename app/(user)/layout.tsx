27f0fbc42d2e6fdbc4df9f90b1e1cf7c81aeb17c
muzaffar401 <ma9400667@gmail.com>
2024-11-26 20:30:15 +0500

    RESUME-BUILDER

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,19 +1,16 @@
-import { auth } from "@/auth"
-import Container from "@/components/Container"
-import Navbar from "@/components/Navbar"
-import { FormProvider } from "@/context/FormContext"
-import React from "react"
+import { auth } from "@/auth";
+import Container from "@/components/Container";
+import Navbar from "@/components/Navbar";
+import { FormProvider } from "@/context/FormContext";
 
 const UserLayout = async ({ children }: { children: React.ReactNode }) => {
-    const session = await auth();
-    return (
-        <FormProvider>
-            <Navbar user={session?.user!} />
-            <Container>
-                {children}
-            </Container>
-        </FormProvider>
-    )
-}
+	const session = await auth();
+	return (
+		<FormProvider>
+			<Navbar user={session?.user!} />
+			<Container>{children}</Container>
+		</FormProvider>
+	);
+};
 
-export default UserLayout
+export default UserLayout;
